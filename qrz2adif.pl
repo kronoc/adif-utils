cab9e5a8823efeccb86e6e5835027b2fdb43ec94
Conor Keegan <conor.keegan@bbc.co.uk>
2014-08-11 08:49:34 +0100

    EOR handling, date and time formatting, trimming empty records

--- qrz2adif.pl
+++ qrz2adif.pl
@@ -7,14 +7,15 @@ my $adifData = "";
 my @fields = (
 	[ "lde", "CALL" ],
 	[ "lba", "BAND" ],
-        [ "lbt", "QSO_DATE" ],
+        [ "ldt", "QSO_DATE" ],
         [ "ltm", "TIME_ON" ],
         [ "lfr", "FREQ" ],
         [ "lmo", "MODE" ],
         [ "lgr", "LOC" ],
-        [ "lcc", "DXCC" ],	
+        [ "lcc\" onclick=", "DXCC" ],
+	[ "lcc\" style=", "OP" ],	
     );
-#handle EOR and DXCC
+
 binmode STDOUT, ':encoding(utf8)';
 binmode STDERR, ':encoding(utf8)';
 
@@ -26,12 +27,28 @@ writeadif($adifData);
 sub extract{
 	my $line = shift(@_);
 	foreach my $field(@fields){
-		if (($line =~ /<td class=\"@$field[0]\" .*\">(.*)<\/td>/) && ($line !~ /<img src.*\/>/)) {
-		return "<@$field[1]:".length($1).">$1\n";
-		};
+		if (($line =~ /<td class=\"@$field[0]\".*\">(.*)<\/td>/) && ($line !~ /<img src.*\/>/)) {
+		if ($1 ne " "){return "<@$field[1]:".length($1).">".clean($1)."\n";}
+		}
+		if ($line =~ /<td class="lac">(.*)<\/td>/) {
+                return "<EOR>\n";
+                }
 	}
 }
 
+sub clean{
+	my $value = shift(@_);
+	if ($value =~ /&#216;/){
+		$value =~ s/&#216;/0/g;
+	}
+	if ($value =~ /\d\d:\d\d/){
+		$value =~ s/(\d\d):(\d\d)/$1$2/g;
+	}
+	if ($value =~ /\d\d\d\d-\d\d-\d\d/){
+                $value =~ s/(\d\d\d\d)-(\d\d)-(\d\d)/$1$2$3/g;
+        }
+	return $value;
+}
 sub writeadif{
 	my $data = shift(@_);
 	print "$data";
